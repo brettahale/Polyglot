c9d1be7b815de1d3cd29abf76b0d03a1cc241961
jimboca <jimboca3@gmail.com>
2016-03-20 11:14:46 -0700

    #1 add check to make sure server does not try to nest primary's 2 levels deep

--- polyglot/nodeserver_api.py
+++ polyglot/nodeserver_api.py
@@ -425,8 +425,11 @@ class NodeServer(object):
         """
         # By default the primary_address is it's own address
         primary_addr = node.address
+        # Unless a Node was passed in as the primary.
         if node.primary is not True:
-            # Unless a Node was passed in as the primary.
+            # A primary node must be it's own primary because ISY only supports one level deep.
+            if not node.primary.primary is True:
+                raise RuntimeError("Node '%s' primary '%s' must be a primary!" % (node.name, node.primary.name))
             primary_addr = node.primary.address;
         self.poly.add_node(node.address, node.node_def_id, primary_addr, node.name)
 
